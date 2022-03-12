e6a7ae16c66c9fa6095221ce291ad4fe1ec77081
Irishikitty <43387410+Irishikitty@users.noreply.github.com>
2022-03-12 16:37:26 -0500

    change pkl dir, del cmt; still has warning

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -1,10 +1,7 @@
-import os
-from data.base_dataset import BaseDataset, get_params, get_transform
-from data.image_folder import make_dataset
-from PIL import Image
+from base_dataset import BaseDataset, get_params, get_transform
 import pickle as pickle
 import torch
-from data.Preprocessing import data_preprocessing_2channel, seed_everything, data_preprocessing_24channel_multi, data_preprocessing_52channel, data_preprocessing_30channel, \
+from Preprocessing import data_preprocessing_2channel, seed_everything, data_preprocessing_24channel_multi, data_preprocessing_52channel, data_preprocessing_30channel, \
     data_preprocessing_24channel_multi_distill
 import numpy as np
 import torch.nn as nn
@@ -29,31 +26,19 @@ class AlignedDataset(BaseDataset):
     """
 
     # def __init__(self, opt):
-    def __init__(self, dir):
+    def __init__(self, dir, size):
         """Initialize this dataset class.
 
         Parameters:
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+            size: box size
         """
         # BaseDataset.__init__(self, opt)
         BaseDataset.__init__(self, dir)
-        # self.dir_AB = os.path.join(opt.dataroot, opt.phase)  # get the image directory
-        # self.AB_paths = sorted(make_dataset(self.dir_AB, opt.max_dataset_size))  # get image paths
-        # assert(self.opt.load_size >= self.opt.crop_size)   # crop_size should be smaller than the size of loaded image
-        # self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
-        # self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
 
-        # self.dir =  opt.dataroot
         self.dir =  dir
-        # print("loading: ", self.dir)
-        self.max_dims = 200
-        # self.all_points = load(self.dir + '/' + 'ligand_env_coords.pickle')
-        self.ligand_atoms_pair = load(self.dir  + 'ligand_env_coords.pickle')
-
-        # self.selected_ligand_atom_pair = load(self.dir + '/' + 'train_selected_ligand_atom_pair.pickle')
-        #
-        # self.ligand_loc_key = torch.tensor([*self.all_points.keys()])
-        # self.len = len(self.ligand_loc_key)
+        self.max_dims = size
+        self.ligand_atoms_pair = load(self.dir  + './datasets/ligand_env_coords.pickle')
         self.len = len(self.ligand_atoms_pair)
 
     def __getitem__(self, index):
@@ -68,22 +53,7 @@ class AlignedDataset(BaseDataset):
             A_paths (str) - - image paths
             B_paths (str) - - image paths (same as A_paths)
         """
-        # read a image given a random integer index
-        # AB_path = self.AB_paths[index]
-        # AB = Image.open(AB_path).convert('RGB')
-        # # split AB image into A and B
-        # w, h = AB.size
-        # w2 = int(w / 2)
-        # A = AB.crop((0, 0, w2, h))
-        # B = AB.crop((w2, 0, w, h))
-        #
-        # # apply the same transform to both A and B
-        # transform_params = get_params(self.opt, A.size)
-        # A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
-        # B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
-        #
-        # A = A_transform(A)
-        # B = B_transform(B)
+
         pdbid, ligand_name, ligand, atoms= self.ligand_atoms_pair[index]
         ligand_loc_list = [ele[1:] for ele in ligand]
         center_loc = np.mean(np.array(ligand_loc_list), axis=0)
@@ -92,12 +62,10 @@ class AlignedDataset(BaseDataset):
         starting_ligand_loc = np.array(ligand_loc_list) + np.array(start_center_loc) - center_loc
         starting_ligand = np.vstack([[22]*len(ligand), starting_ligand_loc.T]).T
 
-        # data_24channel = data_preprocessing_24channel_multi(starting_ligand_loc, atoms,
-        #                                                     ligand)
         tempFakeA, tempTrueB = data_preprocessing_24channel_multi_distill(starting_ligand, atoms, ligand)
 
-        assert tempFakeA.shape[2] <=200
-        assert tempTrueB.shape[2] <= 200
+        assert tempFakeA.shape[2] <= self.max_dims
+        assert tempTrueB.shape[2] <= self.max_dims
 
         if tempFakeA.shape[2] % 2 == 0:
             padding = self.max_dims - tempFakeA.shape[1]
@@ -113,35 +81,12 @@ class AlignedDataset(BaseDataset):
 
         assert A.shape == (24, 250, 250)
         assert B.shape == (24, 250, 250)
-        """
-        
-        A_path = self.dir
-        B_path = self.dir
-        target_keys = self.ligand_loc_key[index]
-        # print(target_keys)
-        #         print(index)
-        # data_2channel = data_preprocessing_2channel(self.all_points, self.selected_ligand_atom_pair, target_keys)
-        # data_52channel = data_preprocessing_52channel(self.all_points, self.selected_ligand_atom_pair, target_keys)
-        
-        # data_30channel = data_preprocessing_30channel(self.all_points, self.selected_ligand_atom_pair, target_keys)
-
-        A = data_24channel[:, :, :55]
-        B = data_24channel[:, :, 55:]
-        A[0, :, :] = (A[0, :, :] / 80) * 2 - 1
-        B[0, :, :] = (B[0, :, :] / 80) * 2 - 1
-
-        # print("-------------------loading---------------------")
-        # print(A.shape, B.shape)
-        """
-        # return {'A': torch.tensor(A).float(), 'B': torch.tensor(B).float(), 'A_paths': A_path, 'B_paths': B_path}
         return {'A': torch.tensor(A).float(), 'B': torch.tensor(B).float(), 'A_paths': "None", 'B_paths': "None",
                 'Ligand_length': len(ligand) , 'env_atoms_length': len(atoms) }
-        # return pdbid, ligand_name, ligand, atoms
 
 
     def __len__(self):
         """Return the total number of images in the dataset."""
-        # return len(self.AB_paths)
         return self.len
 
     # ==============================================================================
@@ -216,7 +161,6 @@ class AlignedDataset(BaseDataset):
                 temp = np.sum(np.exp(np.array(diff)))
                 p_list.append(1 / temp)
 
-            # p_list = softmax(np.array(denominator_ss_list))
             direction = np.random.choice(direction_list, p=p_list)
             next_loc = candidate_d_dict[direction]
 
@@ -227,10 +171,10 @@ class AlignedDataset(BaseDataset):
 
 if __name__=="__main__":
     dir = ""
-    data = AlignedDataset(dir)
+    data = AlignedDataset(dir, 250)
     dataloader = torch.utils.data.DataLoader(
         data,
         batch_size=2,
         shuffle=False)
     a = next(iter(dataloader))
-    print(a)
+    print(a.keys())
